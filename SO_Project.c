91365f0ec7f6608a5f9885e6662b22f1a2645d6f
osvaldodmvs <osvaldodmvs@gmail.com>
2022-04-22 01:08:56 +0100

    funciona mas nao funciona

--- SO_Project.c
+++ SO_Project.c
@@ -19,7 +19,7 @@ typedef struct line{
 }LINE;
 
 int main(int argc, char **argv, char **envp){		// Command Line Arguments
-	char temp[MAX100];
+	char temp[1],buf[100];
 	int fds[2],source,destination,wcf;
 	int N_LINHAS;
 	char buffer[1024],wc[10];
@@ -30,8 +30,8 @@ int main(int argc, char **argv, char **envp){		// Command Line Arguments
 		exit(1);
 	}
 	int number_pids = atoi(argv[1]);
-	int pids[number_pids];
-  	source = open (argv[2], O_RDONLY);
+	pid_t pids[number_pids];
+	source = open (argv[2], O_RDONLY);
 	if (source == -1) {
 		perror ("Opening Source File");
 		exit(1);
@@ -39,14 +39,14 @@ int main(int argc, char **argv, char **envp){		// Command Line Arguments
 	system("wc -l < all_timestamps.csv > /tmp/wc.txt");
 	wcf = open("/tmp/wc.txt",O_RDONLY);
 	if(wcf == -1) {
-        perror ("Opening WC File");
+		perror ("Opening WC File");
 		exit(1);
 	}
 	read(wcf, wc, sizeof(wc));
 	N_LINHAS=atoi(wc);
 	destination = open(argv[3], O_CREAT | O_TRUNC | O_WRONLY, 0666);
 	if (destination == -1) {
-        perror ("Opening Destination File");
+		perror ("Opening Destination File");
 		exit(1);
 	}
 	LINE * linhas = (LINE*)calloc(N_LINHAS,sizeof(LINE)); //alocacao de memoria
@@ -57,29 +57,62 @@ int main(int argc, char **argv, char **envp){		// Command Line Arguments
 	}
 	fscanf(fp,"%*s",temp);
 	printf("LINHAS: %d\n",N_LINHAS);
-	LINE * tmp=linhas;	//apontador temporario para nao desconfigurar apontador inicial da struct
+	LINE * tmpReset=linhas;	//apontador temporario para nao desconfigurar apontador inicial da struct
+	LINE * tmpStamp=linhas;	//apontador temporario para nao desconfigurar apontador inicial da struct
 	for(int i=0;i<N_LINHAS-1;i++){
-		fscanf(fp,"%ld %*[;] %ld %*[;] %ld %*[;] %ld %*[;] %ld %*[\n]",&(tmp->admissao),&(tmp->inicio_triagem),&(tmp->fim_triagem),&(tmp->inicio_medico),&(tmp->fim_medico));
-		tmp++;
+		fscanf(fp,"%ld %*[;] %ld %*[;] %ld %*[;] %ld %*[;] %ld %*[\n]",&(tmpReset->admissao),&(tmpReset->inicio_triagem),&(tmpReset->fim_triagem),&(tmpReset->inicio_medico),&(tmpReset->fim_medico));
+		tmpReset++;
 	}
-	tmp=linhas;	//reset no apontador temporario para a struct
+	tmpReset=linhas;	//reset no apontador temporario para a struct
 	/*LOOP PARA IMPRIMIR TODAS AS OCCORRENCIAS DA STRUCT
 	for(int i=0;i<N_LINHAS-1;i++){
-		printf("LINHA:%d ||| %ld %ld %ld %ld %ld\n",i+2,tmp->admissao,tmp->inicio_triagem,tmp->fim_triagem,tmp->inicio_medico,tmp->fim_medico);
-		tmp++;
+		printf("LINHA:%d ||| %ld %ld %ld %ld %ld\n",i+2,tmpReset->admissao,tmpReset->inicio_triagem,tmpReset->fim_triagem,tmpReset->inicio_medico,tmpReset->fim_medico);
+		tmpReset++;
 	}*/
 	for(int i=0;i<number_pids;i++){ //create child processes
-		if (pids[i]= fork() == -1){
-    		perror("Fork");
-    		exit(1);
-  		} 
-  		int s_admissao=0,s_triagem=0,s_espera=0,s_consulta=0;
-		/*if (pids[i] == 0) { 
-			for(int j=i;j<N_LINHAS-1-i;j++){
-				tmp+=j;
-				if()
+		if ((pids[i]=fork())==-1){
+			perror("Fork");
+			exit(1);
+		}
+		long s_admissao=0,s_triagem=0,s_espera=0,s_consulta=0,timestamp=linhas->admissao;
+		if (pids[i] == 0) { 
+			int mypid=getpid();
+			for(int k=0;N_LINHAS-1;k++){
+				for(int j=i;j<N_LINHAS-1;j+=number_pids){
+					tmpReset+=i;
+					if(tmpReset->admissao < timestamp && timestamp< tmpReset->inicio_triagem){
+						s_admissao++;
+					}
+					if(tmpReset->inicio_triagem < timestamp && timestamp< tmpReset->fim_triagem){
+						s_triagem++;
+					}
+					if(tmpReset->fim_triagem < timestamp && timestamp< tmpReset->inicio_medico){
+						s_espera++;
+					}
+					if(tmpReset->inicio_medico < timestamp && timestamp< tmpReset->fim_medico){
+						s_consulta++;
+					}
+				}
+				tmpReset=linhas;
+				sprintf(buf,"%d$%d,%ld,espera_triagem,%ld\n",mypid,k,timestamp,s_admissao);
+				write(destination,buf,strlen(buf));
+				sprintf(buf,"%d$%d,%ld,sala_triagem,%d\n",mypid,k,timestamp,s_triagem);
+				write(destination,buf,strlen(buf));
+				sprintf(buf,"%d$%d,%ld,sala_espera,%d\n",mypid,k,timestamp,s_espera);
+				write(destination,buf,strlen(buf));
+				sprintf(buf,"%d$%d,%ld,sala_consulta,%d\n",mypid,k,timestamp,s_consulta);
+				write(destination,buf,strlen(buf));
+				//pid$id,timestamp,sala#ocupação 516850
+				tmpStamp++;
+				timestamp=tmpStamp->admissao;
+				s_admissao=s_triagem=s_espera=s_consulta=0;
 			}
-		}*/
-		exit(0); 
+			close(destination);
+			exit(0);
+		}
+		else{
+			wait(NULL);
+		}
 	}
+	return 0;
 }
