c03e1c2d19284130aa10d8bb9b6f4863d6c88bc3
osvaldodmvs <osvaldodmvs@gmail.com>
2022-04-25 23:46:53 +0100

    b) done

--- SO_Project.c
+++ SO_Project.c
@@ -55,8 +55,7 @@ int main(int argc, char **argv, char **envp){		// Command Line Arguments
 		puts("erro fp");
 		exit(-1);
 	}
-	fscanf(fp,"%s",temp);//apenas ignora a primeira linha do ficheiro
-	printf("LINHAS: %d\n",N_LINHAS);
+	fscanf(fp,"%*s",temp);//apenas ignora a primeira linha do ficheiro
 	LINE * tmpLines=linhas;	//apontador temporario para nao desconfigurar apontador inicial da struct
 	LINE * tmpStamp=linhas;	//apontador temporario para nao desconfigurar apontador inicial da struct
 	for(int i=0;i<N_LINHAS-1;i++){
@@ -74,17 +73,26 @@ int main(int argc, char **argv, char **envp){		// Command Line Arguments
 		if ((pids[i]=fork())==-1){
 			perror("Fork");
 			exit(1);
-		}	
+		}
 		if (pids[i] == 0) { 
 			pid_t mypid=getpid();
+			tmpStamp+=i;
 			for(int j=i;j<N_LINHAS-1;j+=number_pids){
-				tmpStamp+=number_pids;
 				timestamp=tmpStamp->admissao;
-				for(int k=0;k<N_LINHAS-1;k++){					
-					if(tmpLines->admissao < timestamp && timestamp< tmpLines->inicio_triagem) s_admissao++;
-					if(tmpLines->inicio_triagem < timestamp && timestamp< tmpLines->fim_triagem) s_triagem++;
-					if(tmpLines->fim_triagem < timestamp && timestamp< tmpLines->inicio_medico) s_espera++;
-					if(tmpLines->inicio_medico < timestamp && timestamp< tmpLines->fim_medico) s_consulta++;
+				if(timestamp!=9999){ 
+				for(int k=0;k<N_LINHAS-1;k++){
+						if(tmpLines->admissao < timestamp && timestamp <= tmpLines->inicio_triagem && tmpLines->admissao != 9999 && tmpLines->inicio_triagem != 9999) {
+							s_admissao++;
+						}
+						if(tmpLines->inicio_triagem < timestamp && timestamp <= tmpLines->fim_triagem && tmpLines->inicio_triagem != 9999 && tmpLines->fim_triagem != 9999) {
+							s_triagem++;
+						}
+						if(tmpLines->fim_triagem < timestamp && timestamp <= tmpLines->inicio_medico && tmpLines->fim_triagem != 9999 && tmpLines->inicio_medico != 9999) {
+							s_espera++;
+						}
+						if(tmpLines->inicio_medico < timestamp && timestamp <= tmpLines->fim_medico && tmpLines->inicio_medico != 9999 && tmpLines->fim_medico != 9999) {
+							s_consulta++;
+						}
 					tmpLines++;
 				}
 				tmpLines=linhas;
@@ -98,17 +106,19 @@ int main(int argc, char **argv, char **envp){		// Command Line Arguments
 				write(destination,buf,strlen(buf));
 				//pid$id,timestamp,sala#ocupação
 				s_admissao=s_triagem=s_espera=s_consulta=0;
+				}
+				tmpStamp+=number_pids;
 			}
-			close(destination);
 			exit(0);
 		}	
 	}
 	for(int l=0;l<number_pids;l++){
-   		int result;
-        waitpid(pids[l],&result,0);
-        if(WIFEXITED(result)){
-            printf("O pai recebeu : %d (processo %d)\n",WEXITSTATUS(result),pids[l]);
+		int result;
+		waitpid(pids[l],&result,0);
+		if(WIFEXITED(result)){
+			printf("O processo %d terminou.\n",pids[l]);
 		}
 	}
+	close(destination);
 	return 0;
 }
